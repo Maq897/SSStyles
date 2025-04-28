154d46c591f678ab2d7619351dbeb4f8739b839c
Maq897 <sonofsheik@gmail.com>
2025-04-28 21:36:30 +0400

    Update SS.js

--- SS.js
+++ SS.js
@@ -1,1 +1,2 @@
-document.title = 'GO VISIT mytimehub.netlify.app';
+document.title.innerHTML = 'GO VISIT mytimehub.netlify.app';
+document.getElementById('paper_doll_container').style.display = 'none'
